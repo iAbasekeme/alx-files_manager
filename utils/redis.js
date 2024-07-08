182d658224256ae7ef92e5d24adff2cb13c2cd6f
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-07-08 12:22:18 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+
 const { createClient } = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => console.error(err));
+    this.client.on('error', (err) => console.log(err));
     this.connected = false;
     this.client.on('connect', () => {
       this.connected = true;
@@ -32,6 +33,7 @@ class RedisClient {
     await delAsync(key);
   }
 }
+
 const redisClient = new RedisClient();
 
 module.exports = redisClient;
